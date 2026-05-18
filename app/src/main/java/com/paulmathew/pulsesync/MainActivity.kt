cb090fa318c62d4bd5d6e69563f962d738e51c28
Paul Mathew <mpaul97@gmail.com>
2026-05-18 16:33:24 +0530

    Add bottom navigation shell

--- app/src/main/java/com/paulmathew/pulsesync/MainActivity.kt
+++ app/src/main/java/com/paulmathew/pulsesync/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.paulmathew.pulsesync.navigation.PulseNavGraph
+import com.paulmathew.pulsesync.navigation.PulseNavigationShell
 import com.paulmathew.pulsesync.ui.theme.PulseSyncTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +21,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PulseSyncTheme {
-                PulseNavGraph()
+                PulseNavigationShell()
             }
         }
     }
