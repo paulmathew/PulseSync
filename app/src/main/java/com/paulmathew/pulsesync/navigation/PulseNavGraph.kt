2c9c48cf0d85dc4f44958bcfa3616dd07b98b005
Paul Mathew <mpaul97@gmail.com>
2026-05-18 13:40:33 +0530

    Initial gradle setup completed base UI package created

---
+++ app/src/main/java/com/paulmathew/pulsesync/navigation/PulseNavGraph.kt
@@ -0,0 +1,2 @@
+package com.paulmathew.pulsesync.navigation
+
--- app/src/main/java/com/paulmathew/pulsesync/ui/theme/Color.kt
+++ app/src/main/java/com/paulmathew/pulsesync/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.paulmathew.pulsesync.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BackgroundPrimary = Color(0xFF0D1117)
+val SurfacePrimary = Color(0xFF161B22)
+val AccentGreen = Color(0xFF00D26A)
+val AccentOrange = Color(0xFFFFA726)
+val AccentRed = Color(0xFFFF6B6B)
+val TextPrimary = Color(0xFFE6EDF3)
+val TextSecondary = Color(0xFF8B949E)
