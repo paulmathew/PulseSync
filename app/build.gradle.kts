a5b5bfb08d7b58a339257e1fd9a1ba8b33faa12a
Paul Mathew <mpaul97@gmail.com>
2026-05-19 13:05:04 +0530

    Enable Hilt dependency injection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+
 }
 
 android {
@@ -67,5 +70,10 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.lifecycle.runtime.compose)
+
+    // Hilt Dependency Injection
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.navigation.compose)
+    annotationProcessor(libs.hilt.compiler) // Note: If using KSP, change this to ksp()
 
 }
